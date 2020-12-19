e704b3e194a0cec100d8cf890f9b8546eb7361a7
julio cabrera <juliocabrera820@gmail.com>
2020-12-19 12:25:09 -0600

    feat: movie routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get '/movies', to: 'movies#index'
-  post '/movies', to: 'movies#create'
+  resources :movies
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
