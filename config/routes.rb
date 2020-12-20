7b6951c0f4be3ba954d376b00e1af6f9f259dffa
julio cabrera <juliocabrera820@gmail.com>
2020-12-19 18:58:49 -0600

    feat: add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
+  concern :rentable do
+    resources :rentals
+  end
+  concern :generable do
+    resources :movie_genres
+  end
   namespace :api do
     namespace :v1 do
-      resources :movies
+      resources :clients, concerns: :rentable
+      resources :vhs, concerns: :rentable
+      resources :movies, concerns: :generable do
+        resources :vhs
+      end
+      resources :genre, concerns: :generable
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
