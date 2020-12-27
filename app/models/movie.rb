88ce7dbfdb312bce5973a50cb64c9d7b3e56c318
julio cabrera <juliocabrera820@gmail.com>
2020-12-26 19:17:24 -0600

    chore: add validation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,6 +3,6 @@ class Movie < ApplicationRecord
   has_one_attached :featured_image
   has_many :vhs
 
-  validates :title, :year, :description, :director, :length, presence: true
+  validates :title, :year, :description, :director, :length, :featured_image, presence: true
   validates :length, numericality: { only_integer: true }
 end
